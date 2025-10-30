f280a6bf8788769dcd7a4221c711741959f14a93
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-10-30 21:42:20 +0200

    React izlabots

--- src/Product.tsx
+++ src/Product.tsx
@@ -1,40 +1,55 @@
 import React from 'react'
-import {useParams} from 'react-router';
-import {Link} from 'react-router';
-import {useState,useEffect} from 'react';
+import { useParams } from 'react-router';
+import { Link } from 'react-router';
+import { useState, useEffect } from 'react';
 
 export default function Product() {
   const { id } = useParams();
 
 
-  const [productData,setproductData] = useState("");
+  const [productData, setproductData] = useState({
+    title: "",
+    description: "",
+    images: [],
+    price:""
+  });
 
-  function loadproductData(productId :any){
-         fetch('https://dummyjson.com/products/'+productId)
-    .then(res => res.json())
-    .then(data => setproductData(data))
-    .finally(()=>{console.log(productData)})
+  function loadproductData(productId: any) {
+    fetch('https://dummyjson.com/products/' + productId)
+      .then(res => res.json())
+      .then(data => setproductData(data))
+      .finally(() => { console.log(productData) })
 
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     loadproductData(id)
-  },[])
-  if(productData){
-  return (
-    <div className='prodctCard'>
-      <Link to="/" className='button'>Atgrizties uz sarakstu</Link>
-      <h1>{productData.title}</h1>
-      <p>{productData.description}</p>
-      <div className='images'>
-        <img src={productData.images[0]} />
+  }, [])
+  if (productData) {
+    return (
+      <div className='prodctCard'>
+        <Link to="/" className='button'>Atgrizties uz sarakstu</Link>
+        <h1>{productData.title}</h1>
+        <p>{productData.description}</p>
+        <div className='images' style={{
+          display:"flex",
+          flexWrap:"wrap",
+          justifyContent:"center",
+          gap:"20px",
+          width:"75%",
+          margin:"auto"
+        }}>
+          {productData.images.map((item,i)=>{
+              return ( <img src={item} key={i} style={{width:"300px"}} />)
+          })}
+         
+
+        </div>
+
+
+
 
       </div>
-
-
-
-
-    </div>
-  )
-}
+    )
+  }
 }
